6a4662e1344c6ce2c128c2c0c7f895ef4f00601a
sambowenhughes <sambowenhughes@hotmail.co.uk>
2016-11-27 14:32:01 +0000

    Test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,28 @@ angular.module('starter.controllers', [])
 })
 
 .controller('SetupCtrl', function($scope) {
+
+    $scope.loginToFacebook = function() {
+      var fbLoginSuccess = function (userData) {
+        alert("Successful login");
+        var userID = userData.authResponse.userID;
+        facebookConnectPlugin.api(userID+"/?fields=id,email", ["user_birthday"],
+          function (result) {
+              alert(result.email);
+          },
+          function (error) {
+              alert("Failed: " + error);
+          });
+      };
+
+      facebookConnectPlugin.login(["email", "public_profile"], fbLoginSuccess,
+        function loginError (error) {
+          $scope.status = "ERROR connecting";
+          alert("ERROR Connecting")
+        }
+      );
+
+    };
 })
 
 .controller('SettingsCtrl', function($scope) {
