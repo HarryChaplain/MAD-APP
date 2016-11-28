df45cc9ea11fdf41190e417d6a0a5ad120870ad4
Saneet Bhella <SaneetBhella@Sunnys-MacBook-Pro.local>
2016-11-28 14:26:10 +0000

    Dropbox and twitter login intergration

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,22 @@ angular.module('starter.controllers', [])
 .controller('PostCtrl', function($scope) {
 })
 
-.controller('SetupCtrl', function($scope) {
+.controller('SetupCtrl', function($scope, $cordovaOauth, $twitterApi, $http) {
+  
+  //twitter & dropbox key information
+  var appKey = '4umc6p0v84tdsxu'; //dropbox key
+  var clientId = '1HTt2CDZ9T8E9Swqk0zeNewJ3'; //twitter client id
+  var clientSecret = 'nWXiDdFMRi6SpamRHvvc0WFSqX0vLbhp9PCUxMB0YWf6vN6QSm'; //twitter client secret
+  
+  //dropbox token information
+  var dropboxKey = 'STORAGE.DROPBOX.KEY';
+  $scope.dropboxToken = JSON.parse(window.localStorage.getItem(dropboxKey));
+  console.log($scope.dropboxToken);
+  
+  //twitter token information
+  var twitterKey = 'STORAGE.TWITTER.KEY';
+  $scope.twitterToken = JSON.parse(window.localStorage.getItem(twitterKey));
+//  console.log($scope.twitterToken);
 
   $scope.loggedIn = false;
     $scope.loginToFacebook = function() {
@@ -40,6 +55,52 @@ angular.module('starter.controllers', [])
         }
       );
 
+    };
+  
+    //*******************************************
+    // Twitter User Setup
+    //*******************************************
+    $scope.loginToTwitter = function() {
+      if ($scope.twitterToken === '' || $scope.twitterToken === null) {
+        $cordovaOauth.twitter(clientId, clientSecret).then(function(result) {
+          $scope.twitterToken = result;
+          window.localStorage.setItem(twitterKey, JSON.stringify(result));
+        }, function(error) {
+          console.log(error);
+        });
+      } 
+      else {
+        console.log("twitter already logged in!");
+        console.log($scope.twitterToken);
+      }
+    };
+  
+    $scope.logoutOfTwitter = function() {
+      localStorage.removeItem(twitterKey);
+      $scope.twitterToken = null;
+    };
+  
+    //*******************************************
+    // Dropbox User Setup
+    //******************************************* 
+    $scope.loginToDropbox = function() {
+      if ($scope.dropboxToken === '' || $scope.dropboxToken === null) {
+        $cordovaOauth.dropbox(appKey).then(function(result) {
+          $scope.dropboxToken = result;
+          window.localStorage.setItem(dropboxKey, JSON.stringify(result));
+        }, function(error) {
+          console.log(error);
+        });
+      } 
+      else {
+        console.log("dropbox already logged in!");
+        console.log($scope.dropboxToken);
+      }
+    };
+  
+    $scope.logoutOfDropbox = function() {
+      localStorage.removeItem(dropboxKey);
+      $scope.dropboxToken = null;
     };
 })
 
